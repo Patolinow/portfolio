60a1d5d966004e414cac20eda9638b342403b119
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-08-10 21:24:25 -0300

    ✨ - Added a Main Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   )
 }
---
+++ frontend/src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+import HeroSection from "../HeroSection";
+
+const Main = () => {
+  return(<main>
+    <HeroSection />
+  </main>)
+};
+
+export default Main;
