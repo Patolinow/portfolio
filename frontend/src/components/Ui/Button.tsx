6111d97fadb7df986f60b201a09ab5ab57ef8522
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-08-11 22:56:40 -0300

    💄 - Changed some button styles

--- frontend/src/components/Ui/Button.tsx
+++ frontend/src/components/Ui/Button.tsx
@@ -4,6 +4,7 @@ import styles from './Button.module.scss'
 const Button = (props:PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>) => {
   const buttonClass = props.className ? props.className : ''
     return (
+      // TODO: adicionar mais propriedades ao botão
         <button className={`${styles.button} ${styles[buttonClass]}`}>{props.children}</button>
     )
 }
