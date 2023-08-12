d75452602c75cd9e536dd81cfc3f2e76f301a59e
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-08-11 23:35:42 -0300

    ✨ - Added more content to HeroSection

--- frontend/src/components/HeroSection/index.tsx
+++ frontend/src/components/HeroSection/index.tsx
@@ -1,19 +1,34 @@
-import heroImage from "../../assets/images/drawn1.png"
+import heroImage from "../../assets/images/drawn1.png";
+import downloadImage from "../../assets/icons/download.svg";
+import arrowImage from "../../assets/images/arrow1.svg";
+import styles from "./HeroSection.module.scss";
 import Button from "../Ui/Button";
 
-const HeroSection = () => {
-  return (<section>
-    <article>
-      <h1>FRONTEND</h1>
-      <h1>DEVELOPER</h1>
+// 
 
-      <div>
-        <Button>Contratar</Button>
-        <Button className="secondary">Baixar CV</Button>
-      </div>
-    </article>
-    <img src={heroImage} alt="" />
-  </section>);
-}
+const HeroSection = () => {
+  return (
+    <section className={styles.home}>
+      <article>
+        <img src={arrowImage} alt="Seta apontando para botão 'contratar'" />
+
+        <div>
+          <h1>FRONTEND</h1>
+          <h1 className="accent-text">DEVELOPER</h1>
+
+          <div className={styles.buttons}>
+            <Button>Contratar</Button>
+            <Button className="secondary"><p>Baixar CV</p> <img src={downloadImage}/></Button>
+          </div>
+        </div>
+        
+
+      </article>
+
+      <img src={heroImage} alt="Imagem de um homem sentado no chão usando um notebook" />
+
+    </section>
+  );
+};
 
 export default HeroSection;
