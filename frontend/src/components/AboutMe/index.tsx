92b04511aa98f016f1205b109b02c922ef2039aa
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-08-18 23:53:21 -0300

    💬  - Added more texts and images to aboutMe

--- frontend/src/components/AboutMe/index.tsx
+++ frontend/src/components/AboutMe/index.tsx
@@ -1,17 +1,24 @@
 import styles from './AboutMe.module.scss';
+import aboutMeImg from '../../assets/images/drawn2.png';
+import musicalNote from '../../assets/images/musicalNote.svg';
+import lightBulb from '../../assets/images/lightbulb.svg';
+// TODO: Animar zzz e adicionar parallax ao lightbulb e musical note
 
 const AboutMe = () => {
   return (
     <section className={styles["about-me"]}>
+      <img src={aboutMeImg} alt="Homem sentado em uma cadeira de escritório enquanto dorme com notebook em cima da mesa em frente a ele" />
+
       <article>
-        <h1>Sobre mim</h1>
+        <h2>Sobre <span className='accent-text'>mim</span></h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem? Quae, quia.
-          Quam, voluptas. Quia, voluptatum? Quia, voluptatum. Quisquam, voluptatem? Quae, quia.
-          Quam, voluptas. Quia, voluptatum? Quia, voluptatum.
+        Olá, meu nome é Fábio e sou um estudante de Engenharia de Software na UNOPAR, fiz curso técnico integrado ao ensino médio na instituição IFBaiano Campus Governador Mangabeira. Eu desenvolvo soluções em React.js utilizando Typescript como linguagem principal, Redux para controle de estado global, CSS Modules, Styled Components, SASS ou Tailwind para estilização dos projetos e atualmente estou aprimorando minhas habilidades em Next.js, aprendendo a utilizar SSR, SSG e ISR de forma Eficiente. 
         </p>
+
       </article>
-      <img src="https://picsum.photos/200" alt="random" />
+
+        <img src={musicalNote} id={styles["musical-note"]} />
+        <img src={lightBulb} id={styles["light-bulb"]} />
     </section>
   );
 };
