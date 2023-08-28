5295d7164dda53a9e8863d1535c5c059540cfe12
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-08-27 23:36:29 -0300

    💄 - Added a tertiary color and linted Button code

--- frontend/src/components/Ui/Button.tsx
+++ frontend/src/components/Ui/Button.tsx
@@ -1,12 +1,12 @@
-import {PropsWithChildren, ButtonHTMLAttributes} from 'react'
-import styles from './Button.module.scss'
+import { PropsWithChildren, ButtonHTMLAttributes } from "react";
+import styles from "./Button.module.scss";
 
-const Button = (props:PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>) => {
-  const buttonClass = props.className ? props.className : ''
-    return (
-      // TODO: adicionar mais propriedades ao botão
-        <button className={`${styles.button} ${styles[buttonClass]}`}>{props.children}</button>
-    )
-}
+const Button = (props: PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>) => {
+  const buttonClass = props.className ? props.className : "";
+  return (
+    // TODO: adicionar mais propriedades ao botão
+    <button className={`${styles.button} ${styles[buttonClass]}`}>{props.children}</button>
+  );
+};
 
-export default Button
+export default Button;
