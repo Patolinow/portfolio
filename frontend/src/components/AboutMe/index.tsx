98ee9443f9343485eb0812a5e04c2497b74cadb5
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-08-28 19:45:13 -0300

    🎨 - FIXED Code formatting from AboutMe

--- frontend/src/components/AboutMe/index.tsx
+++ frontend/src/components/AboutMe/index.tsx
@@ -1,24 +1,33 @@
-import styles from './AboutMe.module.scss';
-import aboutMeImg from '../../assets/images/drawn2.png';
-import musicalNote from '../../assets/images/musicalNote.svg';
-import lightBulb from '../../assets/images/lightbulb.svg';
-// TODO: Animar zzz e adicionar parallax ao lightbulb e musical note
+import styles from "./AboutMe.module.scss";
+import aboutMeImg from "../../assets/images/drawn2.png";
+import musicalNote from "../../assets/images/musicalNote.svg";
+import lightBulb from "../../assets/images/lightbulb.svg";
+// TODO: Animar o zzz e adicionar parallax ao lightbulb e musical note
 
 const AboutMe = () => {
   return (
     <section className={styles["about-me"]}>
-      <img src={aboutMeImg} alt="Homem sentado em uma cadeira de escritório enquanto dorme com notebook em cima da mesa em frente a ele" />
+      <img
+        src={aboutMeImg}
+        alt="Homem sentado em uma cadeira de escritório enquanto dorme com notebook em cima da mesa em frente a ele"
+      />
 
       <article>
-        <h2>Sobre <span className='accent-text'>mim</span></h2>
+        <h2>
+          Sobre <span className="accent-text">mim</span>
+        </h2>
         <p>
-        Olá, meu nome é Fábio e sou um estudante de Engenharia de Software na UNOPAR, fiz curso técnico integrado ao ensino médio na instituição IFBaiano Campus Governador Mangabeira. Eu desenvolvo soluções em React.js utilizando Typescript como linguagem principal, Redux para controle de estado global, CSS Modules, Styled Components, SASS ou Tailwind para estilização dos projetos e atualmente estou aprimorando minhas habilidades em Next.js, aprendendo a utilizar SSR, SSG e ISR de forma Eficiente. 
+          Olá, meu nome é Fábio e sou um estudante de Engenharia de Software na UNOPAR, fiz curso
+          técnico integrado ao ensino médio na instituição IFBaiano Campus Governador Mangabeira. Eu
+          desenvolvo soluções em React.js utilizando Typescript como linguagem principal, Redux para
+          controle de estado global, CSS Modules, Styled Components, SASS ou Tailwind para
+          estilização dos projetos e atualmente estou aprimorando minhas habilidades em Next.js,
+          aprendendo a utilizar SSR, SSG e ISR de forma Eficiente.
         </p>
-
       </article>
 
-        <img src={musicalNote} id={styles["musical-note"]} />
-        <img src={lightBulb} id={styles["light-bulb"]} />
+      <img src={musicalNote} id={styles["musical-note"]} />
+      <img src={lightBulb} id={styles["light-bulb"]} />
     </section>
   );
 };
