02683ba9bca9920143e7ac8601b5be5ff7a79ffe
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-09-01 23:56:55 -0300

    ✏️ - FIXED types from ListItemCard

--- frontend/src/components/Ui/ListItemCard.tsx
+++ frontend/src/components/Ui/ListItemCard.tsx
@@ -1,7 +1,7 @@
-import { PropsWithChildren, HTMLAttributes } from "react";
+import { PropsWithChildren } from "react";
 import styles from "./ListItemCard.module.scss";
 
-const ListItemCard = (props: PropsWithChildren<HTMLAttributes<HTMLUListElement>>) => {
+const ListItemCard = (props: PropsWithChildren<JSX.IntrinsicElements["li"]>) => {
   return <li className={`${styles.Card} ${props.className ?? ""}`}>{props.children}</li>;
 };
 
