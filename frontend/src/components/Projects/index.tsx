39c60d96f06fc57b87943e62c05ceaec18fd9392
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-09-05 23:49:13 -0300

    ✨ - ADDED the new custom hook to the Skills and Projects

--- frontend/src/components/Projects/index.tsx
+++ frontend/src/components/Projects/index.tsx
@@ -2,50 +2,56 @@ import Button from "../Ui/Button";
 import ListItemCard from "./../Ui/ListItemCard";
 import testImg from "./../../assets/images/dt-money.png";
 import styles from "./Projects.module.scss";
+import { useRef } from "react";
+import useImageSlider from "../../hooks/use-ImageSlider";
 
 const Projects = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const slider = useImageSlider(containerRef);
+
   return (
-    <ul id={styles.projects}>
-      <ListItemCard>
-        <img src={testImg} />
-
-        <ul>
-          <li>React</li> <li>Typescript</li> <li>Tailwind</li>
-        </ul>
-
-        <menu>
-          <Button>Preview</Button>
-
-          <Button className="tertiary">Code</Button>
-        </menu>
-      </ListItemCard>
-      <ListItemCard>
-        <img src={testImg} />
-
-        <ul>
-          <li>React</li> <li>Typescript</li> <li>Tailwind</li>
-        </ul>
-
-        <menu>
-          <Button>Preview</Button>
-
-          <Button className="tertiary">Code</Button>
-        </menu>
-      </ListItemCard>
-      <ListItemCard>
-        <img src={testImg} />
-
-        <ul>
-          <li>React</li> <li>Typescript</li> <li>Tailwind</li>
-        </ul>
-
-        <menu>
-          <Button>Preview</Button>
-
-          <Button className="tertiary">Code</Button>
-        </menu>
-      </ListItemCard>
-    </ul>
+    <div
+      className="ul-wrapper"
+      ref={containerRef}
+      onTouchStart={slider.touchstartHandler}
+      onMouseDown={slider.mouseDownHandler}
+      onMouseMove={slider.mouseMoveHandler}
+      onMouseUp={slider.mouseUpHandler}
+      onMouseLeave={slider.mouseLeaveHandler}
+    >
+      <ul id={styles.projects}>
+        <ListItemCard>
+          <img src={testImg} draggable={false}/>
+          <ul>
+            <li>React</li> <li>Typescript</li> <li>Tailwind</li>
+          </ul>
+          <menu>
+            <Button>Preview</Button>
+            <Button className="tertiary">Code</Button>
+          </menu>
+        </ListItemCard>
+        <ListItemCard>
+          <img src={testImg} draggable={false}/>
+          <ul>
+            <li>React</li> <li>Typescript</li> <li>Tailwind</li>
+          </ul>
+          <menu>
+            <Button>Preview</Button>
+            <Button className="tertiary">Code</Button>
+          </menu>
+        </ListItemCard>
+        <ListItemCard>
+          <img src={testImg} draggable={false}/>
+          <ul>
+            <li>React</li> <li>Typescript</li> <li>Tailwind</li>
+          </ul>
+          <menu>
+            <Button>Preview</Button>
+            <Button className="tertiary">Code</Button>
+          </menu>
+        </ListItemCard>
+      </ul>
+    </div>
   );
 };
 
--- frontend/src/components/Skills/index.tsx
+++ frontend/src/components/Skills/index.tsx
@@ -1,5 +1,7 @@
 import styles from "./Skills.module.scss";
 import ListItemCard from "../Ui/ListItemCard";
+import { useRef } from "react";
+import useImageSlider from "../../hooks/use-ImageSlider";
 
 interface ILanguage {
   name: string;
@@ -8,6 +10,8 @@ interface ILanguage {
 }
 
 const Skills = () => {
+  //? languages displayer
+  const languagesArray: ILanguage[] = [];
   const languages = [
     "nextJS",
     "react",
@@ -23,8 +27,6 @@ const Skills = () => {
     "css",
   ];
 
-  const languagesArray: ILanguage[] = [];
-
   languages.forEach((language) => {
     const upperCaseLanguages = ["css", "html", "sass"];
     languagesArray.push({
@@ -34,15 +36,29 @@ const Skills = () => {
     });
   });
 
+  //? item animation
+  const containerRef = useRef<HTMLDivElement>(null);
+  const slider = useImageSlider(containerRef);
+
   return (
-    <ul id={styles.skills}>
-      {languagesArray.map((language) => (
-        <ListItemCard key={language.name}>
-          <img src={language.image} alt={language.name} />
-          <p>{language.isUpperCase ? language.name.toUpperCase() : language.name}</p>
-        </ListItemCard>
-      ))}
-    </ul>
+    <div
+      className="ul-wrapper"
+      ref={containerRef}
+      onTouchStart={slider.touchstartHandler}
+      onMouseDown={slider.mouseDownHandler}
+      onMouseLeave={slider.mouseLeaveHandler}
+      onMouseMove={slider.mouseMoveHandler}
+      onMouseUp={slider.mouseUpHandler}
+    >
+      <ul id={styles.skills}>
+        {languagesArray.map((language) => (
+          <ListItemCard key={language.name}>
+            <img draggable="false" src={language.image} alt={language.name} />
+            <p>{language.isUpperCase ? language.name.toUpperCase() : language.name}</p>
+          </ListItemCard>
+        ))}
+      </ul>
+    </div>
   );
 };
 
