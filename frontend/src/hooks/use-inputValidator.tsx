b90c887e19855a280169d8539073cf35b0754838
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-09-09 23:53:36 -0300

    ✨ - ADDED a custom hook to validate inputs

---
+++ frontend/src/hooks/use-inputValidator.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+interface InputValidatorProps {
+  setInput: React.Dispatch<React.SetStateAction<string>>;
+  errorCondition: boolean;
+}
+
+const useInputValidator = ({setInput, errorCondition}:InputValidatorProps) => {
+  const [inputIsTouched, setInputIsTouched] = useState(false);
+  const isInvalid = !(!errorCondition && inputIsTouched);
+  const borderColor = isInvalid ? "" : "invalid";
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setInput(() => e.target.value);
+  };
+
+  const blurHandler = () => {
+    setInputIsTouched(true);
+  };
+
+  const resetHandler = () => {
+    setInput("");
+    setInputIsTouched(false);
+  }
+
+  return { changeHandler, blurHandler, isInvalid, resetHandler, borderColor};
+}
+
+export default useInputValidator;
