95116f623db1b2d93da8dfbe19f4d9689e2173cf
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-09-10 01:40:53 -0300

    ✨ - ADDED a form handler

--- frontend/src/components/Contact/index.tsx
+++ frontend/src/components/Contact/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { send } from "@emailjs/browser";
+import useInputValidator from "../../hooks/use-inputValidator";
 import contactImage from "../../assets/images/drawn3.png";
 import keyboardImage from "../../assets/images/keyboard.svg";
 import mailImage from "../../assets/images/mail.svg";
@@ -6,6 +9,48 @@ import Button from "../Ui/Button";
 import "./Contact.scss";
 
 const Contact = () => {
+  const [nameInput, setNameInput] = useState("");
+  const nameErrorCondition = nameInput.length >= 2 && nameInput.length < 100;
+  const name = useInputValidator({ errorCondition: nameErrorCondition, setInput: setNameInput });
+
+  const [emailInput, setEmailInput] = useState("");
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  const emailErrorCondition = emailRegex.test(emailInput) && emailInput.length <= 150;
+  const email = useInputValidator({ errorCondition: emailErrorCondition, setInput: setEmailInput });
+
+  const [messageInput, setMessageInput] = useState("");
+  const messageErrorCondition = messageInput.length >= 10 && messageInput.length <= 2000;
+  const message = useInputValidator({
+    errorCondition: messageErrorCondition,
+    setInput: setMessageInput,
+  });
+
+  const formIsInvalid = name.isInvalid && email.isInvalid && message.isInvalid;
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!formIsInvalid) return;
+
+    send(
+      "port_mail_service",
+      "port_template",
+      { user_name: nameInput, user_email: emailInput, message: messageInput },
+      "w8Uj9kvQdVnLY9UR6"
+    )
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+      })
+      .catch((err) => {
+        console.log("FAILED...", err);
+      });
+
+    name.resetHandler();
+    email.resetHandler();
+    message.resetHandler();
+  };
+
   return (
     <section id="contact">
       <article>
@@ -15,25 +60,49 @@ const Contact = () => {
         <img src={contactImage} alt="Homem subindo degrais de um gráfico" />
       </article>
 
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={submitHandler}>
         <fieldset>
           <div id="name-container">
             <label htmlFor="name">Seu nome</label>
             <br />
-            <input type="text" placeholder="Name" />
+            <input
+              onChange={name.changeHandler}
+              onBlur={name.blurHandler}
+              className={name.borderColor}
+              type="text"
+              name="user_name"
+              placeholder="Name"
+              value={nameInput}
+            />
           </div>
 
           <div id="email-container">
             <label htmlFor="email">Seu email</label>
             <br />
-            <input type="text" placeholder="Email" />
+            <input
+              onChange={email.changeHandler}
+              onBlur={email.blurHandler}
+              className={email.borderColor}
+              type="text"
+              name="user_email"
+              placeholder="example@email.com"
+              value={emailInput}
+            />
           </div>
         </fieldset>
 
         <div className="message-cotainer">
           <label htmlFor="message">Sua mensagem</label>
           <br />
-          <textarea placeholder="Message" form="contact-form" />
+          <textarea
+            onChange={message.changeHandler}
+            onBlur={message.blurHandler}
+            className={message.borderColor}
+            placeholder="Message"
+            name="message"
+            form="contact-form"
+            value={messageInput}
+          />
         </div>
 
         <br />
