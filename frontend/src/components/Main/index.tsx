4c02189bc0e12feac4414c6924fa0ecbd66d63d0
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-09-21 21:52:30 -0300

    🎨 - ADDED random imgs to main, instead of about-me

--- frontend/src/components/Main/index.tsx
+++ frontend/src/components/Main/index.tsx
@@ -3,12 +3,16 @@ import AboutMe from "../AboutMe";
 import "./Main.scss";
 import SkillsAndProjects from "../SkillsAndProjects";
 import Contact from './../Contact/index';
+import musicalNote from "../../assets/images/musicalNote.svg";
+import lightBulb from "../../assets/images/lightbulb.svg";
 
 const Main = () => {
   return (
     <main>
       <HeroSection />
+      <img src={musicalNote} id="musical-note" />
       <AboutMe />
+      <img src={lightBulb} id="light-bulb" />
       <SkillsAndProjects />
       <Contact />
     </main>
