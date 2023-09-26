31e9363e65f45e41fa722d2e562e47f3d37e4e48
Patolinow <65193403+Patolinow@users.noreply.github.com>
2023-09-26 19:01:23 -0300

    ✨ - ADDED return to top and random imgs

--- frontend/src/components/Main/index.tsx
+++ frontend/src/components/Main/index.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "../HeroSection";
 import AboutMe from "../AboutMe";
-import "./Main.scss";
 import SkillsAndProjects from "../SkillsAndProjects";
 import Contact from './../Contact/index';
 import musicalNote from "../../assets/images/musicalNote.svg";
 import lightBulb from "../../assets/images/lightbulb.svg";
+import arrowUp from "../../assets/icons/arrow-up.svg";
+import "./Main.scss";
+
 
 const Main = () => {
   return (
@@ -15,6 +17,10 @@ const Main = () => {
       <img src={lightBulb} id="light-bulb" />
       <SkillsAndProjects />
       <Contact />
+      <a href="#" id="return-top">
+        <img src={arrowUp} alt="retornar ao topo" />
+        
+      </a>
     </main>
   );
 };
